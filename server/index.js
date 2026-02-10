22ab312355b697db9c0318b0d84e05a1c656bd7d
Taimur Aftab <taimurmirza@live.com>
2026-02-10 22:52:13 +0000

    Fix CORS: updated allowed origin URL

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "https://theorypro.onrender.com/",
+  "https://theorypro.onrender.com",
 ];
 app.use(
   cors({
